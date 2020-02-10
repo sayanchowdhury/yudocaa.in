e56d8f51c954f3af27247f241d37790834d606dd
Sayan Chowdhury <sayan.chowdhury2012@gmail.com>
2020-02-10 15:48:56 +0530

    i'll render the public files in local
    Signed-off-by: Sayan Chowdhury <sayan.chowdhury2012@gmail.com>

---
+++ public/path----2d05952a6d9279889027.js
@@ -0,0 +1,2 @@
+webpackJsonp([60335399758886],{99:function(t,a){t.exports={data:{site:{siteMetadata:{title:"Sayan Chowdhury's Website"}}},layoutContext:{}}}});
+//# sourceMappingURL=path----2d05952a6d9279889027.js.map
